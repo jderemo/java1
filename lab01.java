2e9da828c015c1429c62f8f245f350e2f6df9d6b
Jeffrey DeRemo <112740205+jderemo@users.noreply.github.com>
2022-09-07 15:23:17 -0400

    Add files via upload

---
+++ lab01.java
@@ -0,0 +1,18 @@
+//jeffrey deremo
+public class lab01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("( `^` ))");
+System.out.println("|     ||");
+System.out.println("|     ||");
+System.out.println("'-----'`");
+
+System.out.println("( `^` ))     ( `^` ))");
+System.out.println("|     ||     |     ||");
+System.out.println("|     ||     |     ||");
+System.out.println("'-----'`     '-----'`");
+
+	}
+
+}
